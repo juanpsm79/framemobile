ef2968fb22eb161f7eddc111956abb040441503c
juan <juanpsm@hotmail.com>
2015-11-05 13:48:23 +0100

    commit setter, getter

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -3,4 +3,12 @@ package com.example.github;
 public class AdapterNotas {
 	public String nota = new String();
 
+	public String getNota() {
+		return nota;
+	}
+
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
+
 }
