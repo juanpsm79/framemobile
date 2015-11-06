5012777e5a1a6e2a521f048eac81ba6cc6278b3a
juan <juanpsm@hotmail.com>
2015-11-06 09:32:52 +0100

    creado campo cocoa

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -1,5 +1,15 @@
 package com.example.github;
 
 public class Cocoa {
+	
+	private String cocoa = null;
+
+	public String getCocoa() {
+		return cocoa;
+	}
+
+	public void setCocoa(String cocoa) {
+		this.cocoa = cocoa;
+	}
 
 }
