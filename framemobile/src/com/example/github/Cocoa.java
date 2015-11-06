2ae6aa8596852c7abdad01c0d6bd03a662c2b61f
juan <juanpsm@hotmail.com>
2015-11-06 13:24:23 +0100

    ultimo cambio cocoa

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -5,7 +5,13 @@ public class Cocoa {
 	private String cocoa;
 
   
-  public String getChampion() {
+  public String getChampion1() {
+		return champion1;
+	}
+	public void setChampion1(String champion1) {
+		this.champion1 = champion1;
+	}
+public String getChampion() {
 		return champion;
 	}
 	public void setChampion(String champion) {
