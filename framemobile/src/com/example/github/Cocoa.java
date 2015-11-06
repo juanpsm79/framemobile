18c978a5d1164381b989608c1df031dfd453c7f1
juan <juanpsm@hotmail.com>
2015-11-06 13:16:10 +0100

    champion get/set

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -5,7 +5,13 @@ public class Cocoa {
 	private String cocoa;
 
   
-  private String champion = null;
+  public String getChampion() {
+		return champion;
+	}
+	public void setChampion(String champion) {
+		this.champion = champion;
+	}
+private String champion = null;
   private String champion1 = null;
 
 }
