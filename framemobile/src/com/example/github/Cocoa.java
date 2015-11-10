bcb20ebb46b142b4f55aa37dfda42b6f65c1c86f
juan <juanpsm@hotmail.com>
2015-11-10 13:45:24 +0100

    Revert "ultimo cambio cocoa"
    This reverts commit 2ae6aa8596852c7abdad01c0d6bd03a662c2b61f.

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -5,13 +5,7 @@ public class Cocoa {
 	private String cocoa;
 
   
-  public String getChampion1() {
-		return champion1;
-	}
-	public void setChampion1(String champion1) {
-		this.champion1 = champion1;
-	}
-public String getChampion() {
+  public String getChampion() {
 		return champion;
 	}
 	public void setChampion(String champion) {
