e6a46ae016eab4b699cdd763ab94795b49a5185a
juan <juanpsm@hotmail.com>
2015-11-11 11:58:42 +0100

    getters and setters en local master

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -4,6 +4,22 @@ public class AdapterNotas {
 	public String nota = new String();
 	public String nota1 = new String();
 	public String nota2 = new String();
+	public String getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(String nota2) {
+		this.nota2 = nota2;
+	}
+
+	public String getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(String nota3) {
+		this.nota3 = nota3;
+	}
+
 	public String nota3 = new String();
 
 	public String getNota() {
--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -12,7 +12,37 @@ public class Cocoa {
 		this.champion = champion;
 	}
 private String champion = null;
-  private String champion1 = null;
+  public String getCocoa() {
+	return cocoa;
+}
+public void setCocoa(String cocoa) {
+	this.cocoa = cocoa;
+}
+public String getChampion1() {
+	return champion1;
+}
+public void setChampion1(String champion1) {
+	this.champion1 = champion1;
+}
+public String getChampion2() {
+	return champion2;
+}
+public void setChampion2(String champion2) {
+	this.champion2 = champion2;
+}
+public String getChampion3() {
+	return champion3;
+}
+public void setChampion3(String champion3) {
+	this.champion3 = champion3;
+}
+public String getChampion4() {
+	return champion4;
+}
+public void setChampion4(String champion4) {
+	this.champion4 = champion4;
+}
+private String champion1 = null;
   private String champion2 = null;
    private String champion3 = null;
    private String champion4 = null;
