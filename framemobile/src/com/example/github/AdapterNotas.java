4a2ff19950457de0bff7c763ce1aaba8b7b34380
juanpsm79 <juanpsm79@gmail.com>
2015-11-11 17:10:23 +0100

    nota3335

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -2,7 +2,7 @@ package com.example.github;
 
 public class AdapterNotas {
 	public String nota = new String();
-	public String nota1 = new String();
-	public String nota2 = new String();
+	public String nota35 = new String();
+	public String nota33 = new String();
 
 }
