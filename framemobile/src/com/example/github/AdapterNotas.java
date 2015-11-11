b7f9f0d5e547f83c49705ff68dcb9cdf22be322a
juan <juanpsm@hotmail.com>
2015-11-11 12:27:03 +0100

    Revert "removed getters and setters"
    This reverts commit fe2e4910f5cf3b1834f33a7844508302b36b1024.

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -4,5 +4,38 @@ public class AdapterNotas {
 	public String nota = new String();
 	public String nota1 = new String();
 	public String nota2 = new String();
+	public String getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(String nota2) {
+		this.nota2 = nota2;
+	}
+
+	public String getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(String nota3) {
+		this.nota3 = nota3;
+	}
+
+	public String nota3 = new String();
+
+	public String getNota() {
+		return nota;
+	}
+
+	public String getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(String nota1) {
+		this.nota1 = nota1;
+	}
+
+	public void setNota(String nota) {
+		this.nota = nota;
+	}
 
 }
