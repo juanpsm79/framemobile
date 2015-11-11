a39ab3597b1ff0eaf840a9f91975475c0c1d4917
juanpsm79 <juanpsm79@gmail.com>
2015-11-11 11:02:23 +0100

    nota champion4

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -15,5 +15,6 @@ private String champion = null;
   private String champion1 = null;
   private String champion2 = null;
    private String champion3 = null;
+   private String champion4 = null;
 
 }
