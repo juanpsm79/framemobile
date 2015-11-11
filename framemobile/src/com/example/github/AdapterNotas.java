dc66454f091f2009bb4399e1738584fc19d8b30c
juanpsm79 <juanpsm79@gmail.com>
2015-11-11 11:01:55 +0100

    nota tres añadida

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -3,6 +3,8 @@ package com.example.github;
 public class AdapterNotas {
 	public String nota = new String();
 	public String nota1 = new String();
+	public String nota2 = new String();
+	public String nota3 = new String();
 
 	public String getNota() {
 		return nota;
