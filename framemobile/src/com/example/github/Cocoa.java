a523d171117f0fc14572f679b8fa8ef2bd501116
juanpsm79 <juanpsm79@gmail.com>
2015-11-13 14:36:28 +0100

    cambios cocoa 5

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -3,9 +3,9 @@ package com.example.github;
 public class Cocoa {
 
 	private String cocoa2;
-	private String champion14 = null;
-   	private String champion24 = null;
-   	private String champion34 = null;
-   	private String champion44 = null;
+	private String champion15 = null;
+   	private String champion25 = null;
+   	private String champion35 = null;
+   	private String champion45 = null;
 
 }
