9934b210798f0cffc3edef82caf4b0a779c10efa
juan <juanpsm@hotmail.com>
2015-11-13 14:48:06 +0100

    cambio cocoa 7

--- framemobile/src/com/example/github/Cocoa.java
+++ framemobile/src/com/example/github/Cocoa.java
@@ -2,10 +2,9 @@ package com.example.github;
 
 public class Cocoa {
 
-	private String cocoa2;
-	private String champion16 = null;
-   	private String champion26 = null;
-   	private String champion36 = null;
-   	private String champion46 = null;
-
+	private String cocoa7;
+	private String champion17 = null;
+   	private String champion27 = null;
+   	private String champion37 = null;
+   	private String champion47 = null;
 }
