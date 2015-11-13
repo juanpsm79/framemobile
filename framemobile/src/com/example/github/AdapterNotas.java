eb04cd04719a6832d05a18b75e2d32748693bd80
juanpsm79 <juanpsm79@gmail.com>
2015-11-13 09:34:07 +0100

    notas cambiadas

--- framemobile/src/com/example/github/AdapterNotas.java
+++ framemobile/src/com/example/github/AdapterNotas.java
@@ -2,7 +2,7 @@ package com.example.github;
 
 public class AdapterNotas {
 	public String nota = new String();
-	public String nota35 = new String();
-	public String nota33 = new String();
+	public String nota45 = new String();
+	public String nota43 = new String();
 
 }
